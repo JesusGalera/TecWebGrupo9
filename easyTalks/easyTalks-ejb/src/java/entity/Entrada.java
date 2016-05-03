4eb0b36fe3016161d58849fcae4144c3711d6c01
JesusGalera <gsuscullar@gmail.com>
2016-05-03 08:22:38 +0200

    proyecto con los los entity y facade creados

---
+++ easyTalks/easyTalks-ejb/src/java/entity/Entrada.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author VICTOR AXEL
+ */
+@Entity
+@Table(name = "ENTRADA")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Entrada.findAll", query = "SELECT e FROM Entrada e"),
+    @NamedQuery(name = "Entrada.findById", query = "SELECT e FROM Entrada e WHERE e.id = :id"),
+    @NamedQuery(name = "Entrada.findByTexto", query = "SELECT e FROM Entrada e WHERE e.texto = :texto"),
+    @NamedQuery(name = "Entrada.findByFechacracion", query = "SELECT e FROM Entrada e WHERE e.fechacracion = :fechacracion")})
+public class Entrada implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private BigDecimal id;
+    @Size(max = 200)
+    @Column(name = "TEXTO")
+    private String texto;
+    @Column(name = "FECHACRACION")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechacracion;
+    @JoinColumn(name = "PROYECTO_ID", referencedColumnName = "ID")
+    @OneToOne(optional = false)
+    private Proyecto proyectoId;
+    @JoinColumn(name = "USUARIO_ID", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private Usuario usuarioId;
+
+    public Entrada() {
+    }
+
+    public Entrada(BigDecimal id) {
+        this.id = id;
+    }
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Date getFechacracion() {
+        return fechacracion;
+    }
+
+    public void setFechacracion(Date fechacracion) {
+        this.fechacracion = fechacracion;
+    }
+
+    public Proyecto getProyectoId() {
+        return proyectoId;
+    }
+
+    public void setProyectoId(Proyecto proyectoId) {
+        this.proyectoId = proyectoId;
+    }
+
+    public Usuario getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Usuario usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Entrada)) {
+            return false;
+        }
+        Entrada other = (Entrada) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Entrada[ id=" + id + " ]";
+    }
+    
+}
---
+++ easyTalks/easyTalks-ejb/src/java/entity/Usuario.java
@@ -0,0 +1,198 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author VICTOR AXEL
+ */
+@Entity
+@Table(name = "USUARIO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u"),
+    @NamedQuery(name = "Usuario.findById", query = "SELECT u FROM Usuario u WHERE u.id = :id"),
+    @NamedQuery(name = "Usuario.findByNickname", query = "SELECT u FROM Usuario u WHERE u.nickname = :nickname"),
+    @NamedQuery(name = "Usuario.findByNombre", query = "SELECT u FROM Usuario u WHERE u.nombre = :nombre"),
+    @NamedQuery(name = "Usuario.findByApellidos", query = "SELECT u FROM Usuario u WHERE u.apellidos = :apellidos"),
+    @NamedQuery(name = "Usuario.findByEmail", query = "SELECT u FROM Usuario u WHERE u.email = :email"),
+    @NamedQuery(name = "Usuario.findByContraseA", query = "SELECT u FROM Usuario u WHERE u.contraseA = :contraseA")})
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private BigDecimal id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "NICKNAME")
+    private String nickname;
+    @Size(max = 100)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Size(max = 100)
+    @Column(name = "APELLIDOS")
+    private String apellidos;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "EMAIL")
+    private String email;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "CONTRASE\ufffdA")
+    private String contraseA;
+    @JoinTable(name = "PERTENECE", joinColumns = {
+        @JoinColumn(name = "USUARIO_ID", referencedColumnName = "ID")}, inverseJoinColumns = {
+        @JoinColumn(name = "PROYECTO_ID", referencedColumnName = "ID")})
+    @ManyToMany
+    private Collection<Proyecto> proyectoCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "usuarioId")
+    private Collection<Proyecto> proyectoCollection1;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "usuarioId")
+    private Collection<Entrada> entradaCollection;
+
+    public Usuario() {
+    }
+
+    public Usuario(BigDecimal id) {
+        this.id = id;
+    }
+
+    public Usuario(BigDecimal id, String nickname, String email, String contraseA) {
+        this.id = id;
+        this.nickname = nickname;
+        this.email = email;
+        this.contraseA = contraseA;
+    }
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseA() {
+        return contraseA;
+    }
+
+    public void setContraseA(String contraseA) {
+        this.contraseA = contraseA;
+    }
+
+    @XmlTransient
+    public Collection<Proyecto> getProyectoCollection() {
+        return proyectoCollection;
+    }
+
+    public void setProyectoCollection(Collection<Proyecto> proyectoCollection) {
+        this.proyectoCollection = proyectoCollection;
+    }
+
+    @XmlTransient
+    public Collection<Proyecto> getProyectoCollection1() {
+        return proyectoCollection1;
+    }
+
+    public void setProyectoCollection1(Collection<Proyecto> proyectoCollection1) {
+        this.proyectoCollection1 = proyectoCollection1;
+    }
+
+    @XmlTransient
+    public Collection<Entrada> getEntradaCollection() {
+        return entradaCollection;
+    }
+
+    public void setEntradaCollection(Collection<Entrada> entradaCollection) {
+        this.entradaCollection = entradaCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Usuario[ id=" + id + " ]";
+    }
+    
+}
